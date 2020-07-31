11e344fa211b8b9efb5635054e638a2fcb2ef100
Jhersy <jhersyvb@gmail.com>
2020-07-30 20:12:25 -0500

    02 Static list and single property views: 02 List of houses

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import houses from './houses.json'
+import House from '../components/House'
+
 const Index = () => (
   <div>
-    <h1>Nextbnb</h1>
+    <h2>Places to stay</h2>
+
+    <div className="houses">
+      {houses.map((house, index) => {
+        return <House key={index} {...house} />
+      })}
+    </div>
+
+    <style jsx>{`
+      .houses {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 300px 300px;
+        grid-gap: 40px;
+      }
+    `}</style>
   </div>
 )
 
