5cec090d1daa232481eeeaf6472ca6cf5fb40477
Jhersy <jhersyvb@gmail.com>
2020-07-31 18:13:06 -0500

    03 Implement the date picker: 05 Configure the DayPickerInput component

--- components/House.js
+++ components/House.js
@@ -3,16 +3,18 @@ import Link from 'next/link'
 const House = props => {
   return (
     <Link href='/houses/[id]' as={'/houses/' + props.id}>
-      <div>
-        <img src={props.picture} width='100%' alt='House picture' />
-        <p>
-          {props.type} - {props.town}
-        </p>
-        <p>{props.title}</p>
-        <p>
-          {props.rating} ({props.reviewsCount})
-      </p>
-      </div>
+      <a>
+        <div>
+          <img src={props.picture} width='100%' alt='House picture' />
+          <p>
+            {props.type} - {props.town}
+          </p>
+          <p>{props.title}</p>
+          <p>
+            {props.rating} ({props.reviewsCount})
+          </p>
+        </div>
+      </a>
     </Link>
   )
 }
