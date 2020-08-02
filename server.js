ab2337a60c3883b7e0993e508b050e8ae9799e5c
Jhersy <jhersyvb@gmail.com>
2020-08-01 19:06:22 -0500

    05 Set up sessions: 02 Install Passport.js

--- server.js
+++ server.js
@@ -1,21 +1,61 @@
 const express = require('express')
 const next = require('next')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-
-const User = require('./model.js').User
-const sequelize = require('./model.js').sequelize
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const handle = nextApp.getRequestHandler()
 
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+const User = require('./model.js').User
+const sequelize = require('./model.js').sequelize
+
 const sessionStore = new SequelizeStore({
   db: sequelize
 })
 
 sessionStore.sync()
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, async function (email, password, done) {
+  if (!email || !password) {
+    done('Email and password required', null)
+    return
+  }
+
+  const user = await User.findOne({ where: { email: email } })
+
+  if (!user) {
+    done('User not found', null)
+    return
+  }
+
+  const valid = await user.isPasswordValid(password)
+
+  if (!valid) {
+    done('Email and password do not match', null)
+    return
+  }
+
+  done(null, user)
+}))
+
+passport.serializeUser((user, done) => {
+  done(null, user.email)
+})
+
+passport.deserializeUser((email, done) => {
+  User.findOne({ where: { email: email } }).then(user => {
+    done(null, user)
+  })
+})
 
 nextApp.prepare().then(() => {
   const server = express()
@@ -31,7 +71,9 @@ nextApp.prepare().then(() => {
         maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
       },
       store: sessionStore
-    })
+    }),
+    passport.initialize(),
+    passport.session()
   )
 
   server.all('*', (req, res) => {
